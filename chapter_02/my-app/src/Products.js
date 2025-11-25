55587e191ec9ea139ffec6ee17d91d36c252ea83
kabelo385 <kabeloxabendlini385@gmail.com>
2025-11-25 18:45:26 +0200

    first commit

---
+++ chapter_02/my-app/src/Products.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Products extends Component {
+  render() {
+    return (
+      <div>
+        <h2>
+          Products
+        </h2>
+        <h2>
+          Courses
+        </h2>
+      </div>
+    );
+  }
+}
+
+export default Products;
