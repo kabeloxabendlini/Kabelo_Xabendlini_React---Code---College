8ea9bab35a4af5e44378c7c39d2251cefa508804
kabelo385 <kabeloxabendlini385@gmail.com>
2026-01-24 14:21:02 +0200

    Resolve merge conflict: remove .env

--- firebase-todo/src/firebase.js
+++ firebase-todo/src/firebase.js
@@ -1,23 +1,39 @@
+// Import Firebase core function to initialize the app
 import { initializeApp } from "firebase/app";
+
+// Import Firebase Authentication services
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
+
+// Import Firestore database service
 import { getFirestore } from "firebase/firestore";
 
+// Firebase project configuration
+// These values uniquely identify and connect your app to Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyBrKN2jioT_Z0NnSgN2LufhlsFE5r77bbQ",
-  authDomain: "my-app-vite-5861d.firebaseapp.com",
-  projectId: "my-app-vite-5861d",
-  storageBucket: "my-app-vite-5861d.appspot.com",
-  messagingSenderId: "303020129074",
-  appId: "1:303020129074:web:9e638b86ecb6398a37352b",
-  measurementId: "G-38S60FNJV4",
+  apiKey: "AIzaSyBrKN2jioT_Z0NnSgN2LufhlsFE5r77bbQ", // Public API key for Firebase
+  authDomain: "my-app-vite-5861d.firebaseapp.com", // Auth domain for Firebase Authentication
+  projectId: "my-app-vite-5861d", // Firebase project ID
+  storageBucket: "my-app-vite-5861d.appspot.com", // Cloud Storage bucket
+  messagingSenderId: "303020129074", // Sender ID for Firebase Cloud Messaging
+  appId: "1:303020129074:web:9e638b86ecb6398a37352b", // Unique app identifier
+  measurementId: "G-38S60FNJV4", // Google Analytics measurement ID
 };
 
+// Initialize Firebase app using the configuration above
 const app = initializeApp(firebaseConfig);
 
+// Initialize Firebase Authentication and export it for use in the app
 export const auth = getAuth(app);
+
+// Create and export Google authentication provider
+// Used for "Sign in with Google"
 export const googleProvider = new GoogleAuthProvider();
+
+// Initialize and export Firestore database instance
 export const db = getFirestore(app);
 
+// Export the Firebase app instance (optional but useful)
 export default app;
 
+// Deployed app URL
 // https://fir-todo-b76f4.web.app
