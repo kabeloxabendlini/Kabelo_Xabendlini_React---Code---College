bae913ea368340f6dcaa1ec9bd96d14cc9b25d18
kabelo385 <kabeloxabendlini385@gmail.com>
2026-03-30 11:14:19 +0200

    Add django-cloudinary-storage to requirements

--- firebase-todo/src/firebase.js
+++ firebase-todo/src/firebase.js
@@ -10,13 +10,14 @@ import { getFirestore } from "firebase/firestore";
 // Firebase project configuration
 // These values uniquely identify and connect your app to Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyBrKN2jioT_Z0NnSgN2LufhlsFE5r77bbQ", // Public API key for Firebase
-  authDomain: "fir-todo-b76f4.firebaseapp.com",  // ← should say fir-todo-b76f4
-  projectId: "fir-todo-b76f4",                    // ← should say fir-todo-b76f4
-  storageBucket: "fir-todo-b76f4.appspot.com", // Cloud Storage bucket for file uploads
-  messagingSenderId: "303020129074", // Sender ID for Firebase Cloud Messaging
-  appId: "1:303020129074:web:9e638b86ecb6398a37352b", // Unique app identifier
-  measurementId: "G-38S60FNJV4", // Google Analytics measurement ID
+  apiKey: "AIzaSyAYMlbNYYPrRhyjc-yzkvncL1k_gZo1_C8",
+  authDomain: "fir-todo-b76f4.firebaseapp.com",
+  databaseURL: "https://fir-todo-b76f4-default-rtdb.firebaseio.com",
+  projectId: "fir-todo-b76f4",
+  storageBucket: "fir-todo-b76f4.firebasestorage.app",
+  messagingSenderId: "631032473138",
+  appId: "1:631032473138:web:36c30d588304a4e7ddbe2b",
+  measurementId: "G-FMC8D9B0DE"
 };
 
 // Initialize Firebase app using the configuration above
