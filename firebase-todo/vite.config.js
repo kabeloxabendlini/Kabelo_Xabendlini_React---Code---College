a3598da69d1388497c39f84a04a5cf25b093e15e
kabelo385 <kabeloxabendlini385@gmail.com>
2026-03-30 08:30:46 +0200

    Add django-cloudinary-storage to requirements

--- firebase-todo/vite.config.js
+++ firebase-todo/vite.config.js
@@ -3,5 +3,12 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/"
+  base: "/",
+  server: {
+    headers: {
+      // Allows Firebase Auth redirect & popup flows during local dev
+      "Cross-Origin-Opener-Policy": "unsafe-none",
+      "Cross-Origin-Embedder-Policy": "unsafe-none",
+    },
+  },
 });
